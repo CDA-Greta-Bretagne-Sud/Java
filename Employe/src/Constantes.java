96c51ddce4c9b9f3454400d00e6a9e166f6044c8
Casinni <pascal@lamy.mobi>
2022-09-22 14:11:56 +0200

    correction

---
+++ Employe/src/Constantes.java
@@ -0,0 +1,15 @@
+
+public abstract class Constantes {
+
+	final int BASE_SALAIRE=600;
+	
+	final int VALEUR_SMIC=900;
+	
+	final int LISTE_EMPLOYE_RESP=50;
+	
+	
+	public Constantes() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
