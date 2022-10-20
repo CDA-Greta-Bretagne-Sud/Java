697aa9ad154e7fd9de5083908e16bdfdb224ba7b
Casinni <pascal@lamy.mobi>
2022-10-20 15:38:28 +0200

    Correction type enum

---
+++ bankonet-tp-enum/src/com/bankonet/Client.java
@@ -0,0 +1,87 @@
+package com.bankonet;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Client {
+	
+	private Civilite civilite;
+	private String nom;
+	private String prenom;
+	private String identifiant;
+	
+	private Map<String, Compte> comptesMap = new HashMap<>();
+
+	public Client(Civilite civilite, String nom, String prenom, String identifiant) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.identifiant = identifiant;
+		this.civilite = civilite;
+	}
+	
+	public void creerCompte(Compte compte) {
+		comptesMap.put(compte.getNumero(), compte);
+	}
+	
+	public void supprimerCompte(Compte compte) {
+		comptesMap.remove(compte.getNumero());
+	}
+	
+	public Compte retournerCompte(String numero) throws CompteNonTrouveException {
+		Compte compteRecherche = (Compte) comptesMap.get(numero);
+	
+		if(compteRecherche == null) {
+			throw new CompteNonTrouveException();
+		}
+		
+		return compteRecherche;
+	}
+	
+	
+	public void supprimerCompte(String numero) throws CompteNonTrouveException {
+		Compte compteRecherche = retournerCompte(numero);
+		comptesMap.remove(compteRecherche.getNumero());
+	}
+	
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	public Collection<Compte> getComptesList() {
+		return comptesMap.values();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Client [civilite=%s, nom=%s, prenom=%s, identifiant=%s, comptesMap=%s]", civilite.getLibelle(), nom,
+				prenom, identifiant, comptesMap);
+	}
+
+	
+
+	
+
+}
